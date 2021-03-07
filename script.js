79fb3327644cf1766675e56e1336153586ca7d9f
gozdetekalmaz <gozdetekalmaz@gmail.com>
2021-03-07 15:23:10 +0300

    Upload image by using canvas

--- script.js
+++ script.js
@@ -1,59 +1,17 @@
-/* let uploaded_file = document.getElementById('uploaded-file'),
-  preview = document.getElementById('preview');
+document.getElementById('uploaded_file').addEventListener('change', function (e) {
+  const canvas = document.getElementById('img-canvas');
+  const imgContext = document.getElementById('img-canvas').getContext('2d');
 
-uploaded_file.addEventListener('change', function () {
-  uploadImage(this);
+  let img = new Image();
+  // img.style.height = '500px';
+  // img.style.width = '500px';
+  // img.src = e.target.files[0];
+  console.log(e.target.files[0]);
+  img.src = URL.createObjectURL(e.target.files[0]);
+
+  // uploadSec.appendChild(img);
+
+  img.onload = function () {
+    imgContext.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+  };
 });
-
-function uploadImage(input) {
-  let reader;
-
-  if (input.files && input.files[0]) {
-    reader = new FileReader();
-
-    reader.onload = function (e) {
-      preview.setAttribute('src', e.target.result);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-  }
-} */
-
-document
-  .getElementById('uploaded_file')
-  .addEventListener('change', function (e) {
-    let newImage = document.createElement('img');
-
-    newImage.src = e.target.files[0];
-    console.log(e.target.files[0]);
-    newImage.src = URL.createObjectURL(e.target.files[0]);
-
-    output.appendChild(newImage);
-  });
-
-/* function encodeImageFileAsURL() {
-  var filesSelected = document.getElementById('uploaded_file').files;
-  if (filesSelected.length > 0) {
-    let fileToLoad = filesSelected[0];
-
-    let fileReader = new FileReader();
-
-    fileReader.onload = function (fileLoadedEvent) {
-      let srcData = fileLoadedEvent.target.result; // <--- data: base64
-
-      let newImage = document.createElement('img');
-      newImage.src = srcData;
-
-      document.getElementById('imgTest').innerHTML = newImage.outerHTML;
-      alert(
-        'Converted Base64 version is ' +
-          document.getElementById('imgTest').innerHTML
-      );
-      console.log(
-        'Converted Base64 version is ' +
-          document.getElementById('imgTest').innerHTML
-      );
-    };
-    fileReader.readAsDataURL(fileToLoad);
-  }
-} */
