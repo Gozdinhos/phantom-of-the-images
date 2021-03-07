d65dd7ca6a15fbc4b17024a67781448c9ab27f36
gozdetekalmaz <gozdetekalmaz@gmail.com>
2021-03-07 15:26:28 +0300

    Show image in b&w for reducing data

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ document.getElementById('uploaded_file').addEventListener('change', function (e)
     imgContext.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
   };
 });
+
+function showChannels() {
+  const canvas = document.getElementById('img-canvas');
+  const imgContext = document.getElementById('img-canvas').getContext('2d');
+  let imageData = imgContext.getImageData(0, 0, canvas.width, canvas.height);
+  // (0.3 * R) + (0.59 * G) + (0.11 * B)
+  for (let index = 0; index < imageData.data.length; index += 4) {
+    const r = imageData.data[index];
+    const g = imageData.data[index + 1];
+    const b = imageData.data[index + 2];
+    const a = imageData.data[index + 3];
+    const newVal = r * 0.3 + g * 0.59 + b * 0.11;
+    imageData.data[index] = newVal;
+    imageData.data[index + 1] = newVal;
+    imageData.data[index + 2] = newVal;
+  }
+  console.log(imageData);
+  imgContext.putImageData(imageData, 0, 0);
+}
